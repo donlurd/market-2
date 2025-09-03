979a8695145a341f3fdadfe25f675edf3110b3f8
Ariel Alejandro Villegas Rodriguez <donlurdo@gmail.com>
2025-09-03 15:09:04 -0400

    creacion finamrket prueba practica

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Home } from './home/home';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [Home, RouterOutlet],
+  //templateUrl: './app.html',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my');
+  protected readonly title = signal('homes');
 }
---
+++ src/app/char-component/char-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-char-component',
+  imports: [],
+  templateUrl: './char-component.html',
+  styleUrl: './char-component.css'
+})
+export class CharComponent {
+
+}
